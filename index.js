245bae1ea72fb9c5c9895b6f1a1b0e7795efb425
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-11-14 20:49:15 +0530

    get

--- index.js
+++ index.js
@@ -32,3 +32,9 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('*',(req,res,next)=>{
+  res.status(200).json({
+    message:'bad request'
+  })
+})
