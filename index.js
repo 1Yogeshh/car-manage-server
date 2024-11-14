5ac2524b1f844c897d519f8ecbdefe88a0877bb8
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-11-14 20:55:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(cors());
  }));
 
 app.use(express.json());
-app.use('/api/auth', authRoutes);
-app.use('/api/cars', carRoutes);
+app.use('/auth', authRoutes);
+app.use('/cars', carRoutes);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
